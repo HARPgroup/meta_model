c321d67b5f45becae5267b9f5f268878a91d4f90
Robert W. Burgholzer <rburghol@vt.edu>
2023-03-28 19:01:10 +0000

    added model status script

---
+++ scripts/rest/set_model_status.R
@@ -0,0 +1,85 @@
+# This script will take in our hydr csv as an argument and perform analysis on 
+# variables Qout, ps, wd, and demand and pushes to VAhydro.
+# The values calculated are based on waterSupplyModelNode.R
+basepath='/var/www/R';
+source("/var/www/R/config.R")
+
+suppressPackageStartupMessages(library(hydrotools)) 
+
+# Accepting command arguments:
+argst <- commandArgs(trailingOnly = T)
+river_seg <- argst[1]
+scenario_name <- argst[2]
+hydr_file_path <- argst[3]
+model_version <- argst[4]
+rseg_ftype <- argst[5]
+model_status_flag <- argst[6]
+model_status_msg <- argst[7]
+
+# Set up our data source
+ds <- RomDataSource$new(site, rest_uname = rest_uname)
+ds$get_token(rest_pw)
+if (rseg_ftype == 'vahydro') {
+  # we have a hinky prefix, so add it
+  rseg_code=paste0('vahydrosw_wshed_',river_seg)
+} else {
+  rseg_code=river_seg
+}
+
+message(paste("Searching for feature hydrocode=", rseg_code,"with ftype",rseg_ftype))
+riverseg<- RomFeature$new(
+  ds,
+  list(
+    hydrocode=rseg_code,
+    ftype=rseg_ftype,
+    bundle='watershed'
+  ),
+  TRUE
+)
+message(paste("Searching for model", model_version,"on feature",riverseg$hydroid))
+model <- RomProperty$new(
+  ds,
+  list(
+    featureid=riverseg$hydroid, 
+    entity_type="dh_feature", 
+    propcode=model_version
+  ), 
+  TRUE
+)
+if (is.na(model$pid)) {
+  model$propname = paste(riverseg$name, model_version)
+  model$varid = ds$get_vardef('om_water_model_node')$varid
+  model$save(TRUE)
+}
+
+model_scenario <- RomProperty$new(
+  ds,
+  list(
+    featureid=model$pid, 
+    entity_type="dh_properties", 
+    propcode=scenario_name
+  ), 
+  TRUE
+)
+if (is.na(model_scenario$pid)) {
+  model_scenario$propname = paste(scenario_name)
+  model_scenario$varid = ds$get_vardef('om_scenario')$varid
+  model_scenario$save(TRUE)
+}
+
+model_status <- RomProperty$new(
+  ds,
+  list(
+    featureid=model_scenario$pid, 
+    entity_type="dh_properties", 
+    propname='model_status'
+  ), 
+  TRUE
+)
+model_status$propvalue = model_status_flag
+model_status$propcode = model_status_msg
+model_status$save(TRUE)
+
+if (is.na(model_status$pid)) {
+  message(paste("Error: Could not set model status for",river_seg,">",rseg_ftype,">",model_version,">",scenario_name))
+}
