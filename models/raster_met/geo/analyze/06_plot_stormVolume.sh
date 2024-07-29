0fe55120668dbb9df51d1eef54cb7ec5a7b43e77
COV\gcw73279 <connor.brogan@deq.virginia.gov>
2024-07-29 16:50:35 -0400

    Updates to name to match meta model

---
+++ models/raster_met/geo/analyze/06_plot_stormVolume.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# loads the args, the raster specific config and change to temp dir
+geo_config=`find_config geo.config`
+if [ "$geo_config" = "" ]; then
+  geo_config="$META_MODEL_ROOT/models/raster_met/geo/geo.config"
+fi
+. $geo_config
+
+if [[ "$STORMSEP_PLOT" ]]; then
+  exit
+fi
+if [[ "$GEO_MET_MODEL" != "storm_volume" ]]; then
+  exit
+fi
+
+
+#Where are combined flow precip data file stored?
+comp_dataFilePath=$DAILY_PRECIP_FILE
+#Where to get data from process 02-stormAnalysis
+stormStatsPath=$STORM_EVENT_FLOW_FILE
+#Where to get data from process 03-stormStatistics
+stormPath=$STORM_EVENT_STATS_FILE
+# Directory to store plots in. 
+pathToWrite=$STORM_EVENT_PRECIP_PLOT_DIR
+# The USGS gage number or hydro ID of the coverage that will be used to store
+# this data with unique names
+plotDetails=$USGS_GAGE
+
+
+#While plotting, must clear existing plots. Can do so by deleting directory 
+#using -r recursive and -f force, which ignores nonexistant files
+rm -rf $pathToWrite
+#Add directory:
+mkdir $pathToWrite
+
+
+cmd="Rscript $META_MODEL_ROOT/scripts/met/stormEventsLM_cmd.R $comp_dataFilePath $stormStatsPath $stormPath $pathToWrite $plotDetails"
+echo "Running: $cmd"
+eval $cmd
