1546dfc9f181110b95494c127357120172f9cc34
COV\gcw73279 <connor.brogan@deq.virginia.gov>
2024-08-12 16:12:12 -0400

    Added script for full model runs of stormVol

---
+++ scripts/met/runAllStormVol_prism.sh
@@ -0,0 +1,31 @@
+gage_coverage_file="usgsgageList.csv"
+gage_coverage_SQLfile=usgsgageList.sql
+gageSQL="
+\\set fname '/tmp/${gage_coverage_file}' \n
+ 
+
+copy ( select hydrocode
+FROM dh_feature
+WHERE bundle = 'usgsgage'
+LIMIT 2
+) to :'fname';"
+# turn off the expansion of the asterisk
+set -f
+echo -e $gageSQL > $gage_coverage_SQLfile 
+cat $gage_coverage_SQLfile | psql -h dbase2 -d drupal.dh03
+sftp dbase2:"/tmp/${gage_coverage_file}" "/home/cobrogan/${gage_coverage_file}"
+
+filename="/home/cobrogan/${gage_coverage_file}"
+for i in `cat $filename`; do
+	echo $i
+done
+
+filename="/home/cobrogan/${gage_coverage_file}"
+for i in `cat $filename`; do
+  echo "Running: sbatch /opt/model/meta_model/run_model stormVol_prism \"$i\" auto geo"
+  #sbatch /opt/model/meta_model/run_model raster_met stormVol_prism "$i" auto geo 
+done
+
+First one below giving trouble:
+Running: sbatch /opt/model/meta_model/run_model raster_met stormVol_prism "usgs_01613900" auto geo
+Running: sbatch /opt/model/meta_model/run_model raster_met stormVol_prism "usgs_01616100" auto geo
