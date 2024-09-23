1e5719b81a3210625d5ca2f93c90e2c9b9ca0d03
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2024-09-23 12:14:24 -0400

    retrieve data

--- scripts/met/met_store_info.R
+++ scripts/met/met_store_info.R
@@ -3,9 +3,48 @@
 # attach a property with a web URL to the origina raster file
 basepath='/var/www/R';
 source("/var/www/R/config.R")
+library("lubridate")
+library("data")
+# Set up our data source
+ds <- RomDataSource$new(site, rest_uname = rest_uname)
+ds$get_token(rest_pw)
 
-message("Use: Rscript met_store_info.R $ddate $datasource $extent_hydrocode $extent_ftype $img_varkey $met_file")
+# Accepting command arguments:
+argst <- commandArgs(trailingOnly = T)
+if (length(argst) < 7) {
+  
+  message("Use: Rscript met_store_info.R $ddate datasource coverage_hydrocode coverage_bundle coverage_ftype model_version met_file")
+  message("Ex: Rscript met_store_info.R $ddate nldas2 N5113 landunit cbp6_landseg met_file")
+  q('n')
+}
+ddate <- argst[1]
+datasource <- argst[2]
+coverage_hydrocode <- argst[3]
+coverage_bundle <- argst[4]
+coverage_ftype <- argst[5]
+model_version <- argst[6] 
+met_file <- argst[7] 
 # load the feature -- get feature hydroid
 # find the dh_timeseries_weather record for this event
 # attach an image property to the record
 # return 
+met_data <- 
+
+message(paste("Searching for feature hydrocode=", coverage_hydrocode,"with ftype",coverage_ftype))
+feature <- RomFeature$new(
+  ds,
+  list(
+    hydrocode=coverage_hydrocode,
+    ftype=coverage_ftype,
+    bundle=coverage_bundle
+  ),
+  TRUE
+)
+# this will create or retrieve a model scenario to house this summary data.
+model <- om_model_object(feature, model_version)
+# if a matching model does not exist, this will go ahead and create one
+scenario <- om_get_model_scenario(model, data_source)
+
+met_data <- read.table(met_file, header = TRUE, sep=",")
+numrecs <- nrow(met_data)
+vahydro_post_metric_to_scenprop(scenario$pid, 'om_class_Constant', NULL, 'num_records', numrecs, ds)
