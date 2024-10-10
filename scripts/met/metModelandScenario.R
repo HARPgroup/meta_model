7507a3c616a633eb25ebc4b5cee62713d8dd8370
Brogan <connor.brogan@deq.virginia.gov>
2024-10-10 08:51:41 -0400

    minor changes to analyze 9 and 10

--- scripts/met/metModelandScenario.R
+++ scripts/met/metModelandScenario.R
@@ -7,16 +7,15 @@
 # coverage_bundle <- 'watershed'
 # model_version <- 'met1.0'
 # scenarioPropName <- 'simple_lm'
-# ratingsFile <- "C:/Users/gcw73279.COV/Desktop/gitBackups/gitHome/usgs_ws_01668000-nldas-storm_volume-rating-ts.csv"
+# ratingsFile <- "http://deq1.bse.vt.edu:81/met/stormVol_nldas2_tiled/out/usgs_ws_01668000-nldas-storm_volume-rating-ts.csv"
 
 
 #Load in hydrotools and connect to REST
 library(hydrotools)
 basepath='/var/www/R'
 source('/var/www/R/config.R')
-ds <- RomDataSource$new("http://deq1.bse.vt.edu/d.dh", rest_uname)
+ds <- RomDataSource$new(site = site, rest_uname)
 ds$get_token(rest_pw)
-
 
 
 argst <- commandArgs(trailingOnly = T)
@@ -57,26 +56,33 @@ feature <- RomFeature$new(
   ),
   TRUE
 )
+message(paste("Found feature with hydroid =", feature$hydroid))
 
 #Create a name for the model property to be created on the feature:
 model_name <- paste(coverage_hydrocode,model_version)
+
+message(paste("Creating model property on", feature$hydroid, "with propname =",model_name))
 # if a matching model does not exist, this will go ahead and create one using
 # romProperty
 model <- om_model_object(ds, feature, model_version, 
                          model_name = model_name)
-
+message(paste("Model property with propname =",model$propname," created with pid =",model$pid))
 
 # this will create or retrieve a model scenario to house this summary data using
 # romProperty
+message(paste("Creating model scenario on", model$pid, "with propname =",scenarioPropName))
 scenario <- om_get_model_scenario(ds, model, scenarioPropName)
+message(paste("Scenario property with propname =",scenario$propname," created with pid =",scenario$pid))
 
+#Add featureid and entity_type to ratings for proper export to dh_timeseries
 ratings$featureid <- scenario$pid
 ratings$entity_type <- "dh_properties"
-
+#Create a nicely formatted timeseries that will be easy to export to dh_timeseries
 out <- data.frame(tstime = as.integer(ratings$start_date_sec),
                   tsendtime = as.integer(ratings$end_date_sec),
                   tsvalue = ratings$rating,
                   featureid = ratings$featureid,
                   entity_type = ratings$entity_type)
-
+#Write out the formatted timeseries
+message(paste("Writing out formatted timeseries to",pathToWrite))
 write.csv(out,pathToWrite,row.names = FALSE)
