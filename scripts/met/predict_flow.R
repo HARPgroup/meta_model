6cf91628cd9b8c1ff360779f7b802415f0de0de4
Ilona Hughes <ilonah22@vt.edu>
2024-12-06 12:24:08 -0500

    Change date column names

--- scripts/met/predict_flow.R
+++ scripts/met/predict_flow.R
@@ -54,5 +54,7 @@ predicted_data$rating <- 1-(abs(predicted_data$volumeAboveBaseQMG-predicted_data
 
 predicted_data <- predicted_data[,c("startDate","endDate","rating")]
 
+names(predicted_data)<-c("start_date", "end_date", "rating")
+
 # Writing out predicted flow data as csv
 write.csv(predicted_data,pathToWriteData)
