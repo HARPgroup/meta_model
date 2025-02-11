fe18117f21e4c50962f3a9a586fcccaff652a2b4
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2025-02-11 10:51:59 -0500

    add R routine to transform usgs flow to om

---
+++ scripts/usgs/usgs_to_om_flows.R
@@ -0,0 +1,66 @@
+# Inputs (args):
+# 1 = Gage id you want to use
+# 2 = End path of new csv
+suppressPackageStartupMessages(library(dataRetrieval))
+suppressPackageStartupMessages(library(lubridate))
+suppressPackageStartupMessages(library(sqldf))
+suppressPackageStartupMessages(library(dplyr))
+argst <- commandArgs(trailingOnly = TRUE)
+if (length(args) != 2) {
+  message("Usage: Rscript scale_usgs_flows.R sta_id output_path")
+  q()
+}
+
+basepath='/var/www/R'
+source('/var/www/R/config.R')
+gageid <- as.character(args[1])
+outfile <- argst[2]
+if (count(argst) > 2) {
+  mstart <- as.character(args[2])
+} else {
+  mstart = FALSE
+}
+if (count(argst) > 3) {
+  mend <- as.character(args[3])
+} else {
+  mend = FALSE
+}
+area_reach = 0 # we will not scale if not requested
+if (count(argst) > 4) {
+  area_reach <- as.numeric(argst[5]) 
+}
+drop_cache = 0
+if (count(argst) > 5) {
+  drop_cache = as.integer(argst[6])
+}
+#historic <- dataRetrieval::readNWISdv(gageid,'00060')
+if (drop_cache == 1) {
+  drop_cache(memo_readNWISsite)(gageid)
+  drop_cache(memo_readNWISdv)(gageid,'00060')
+}
+historic <- memo_readNWISdv(gageid,'00060')
+gage_info <- memo_readNWISsite(gageid)
+gage_name <- gage_info$station_nm
+area_factor <- as.numeric(area_reach) / gage_info$drain_area_va
+
+if (!is.logical(intake_da)) {
+  # we need to scale both model output and gage output
+  area_factor <- as.numeric(intake_da) / gage_info$drain_area_va
+}
+historic$X_00060_00003 <- historic$X_00060_00003 * area_factor
+#gage_flows <- zoo(as.numeric(as.character( historic$X_00060_00003 )), order.by = as.POSIXct(historic$Date));
+# add dates
+historic[,c('yr', 'mo', 'da')] <- cbind(year(as.Date(flow_data$Date)),
+                                         month(as.Date(flow_data$Date)),
+                                         day(as.Date(flow_data$Date)))
+historic$Qout <- historic$X_00060_00003 * area_factor
+historic$area_sqmi <- gage_info$drain_area_va
+historic$thisdate <- historic$Date
+historic$ps_mgd <- 0.0
+historic$ps_cumulative_mgd <- 0.0
+historic$wd_cumulative_mgd <- 0.0
+historic$Runit <- historic$Qout / gage_info$drain_area_va
+# this needs to create an epoch...
+# historic$timestamp <- as.POSIXct(historic$Date)
+
+write.csv(historic, outfile, row.names=FALSE)
