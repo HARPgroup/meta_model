de789d05b26e5d08f1fd1a7367dcac02d7a33105
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2025-02-11 19:47:45 -0500

    adapt run id names

--- tests/om_gage_model.R
+++ tests/om_gage_model.R
@@ -1,10 +1,12 @@
-rseg_hydrocode = 'vahydrosw_wshed_PM7_4581_4580'
+rseg_hydrocode = 'vahydrosw_wshed_PS5_5240_5200'
 riverseg_feature <- RomFeature$new(ds, list(hydrocode=rseg_hydrocode,bundle='watershed',ftype='vahydro'), TRUE)
 rseg_model <- RomProperty$new(ds, list(featureid=riverseg_feature$hydroid, propcode='vahydro-1.0', entity_type='dh_feature' ), TRUE)
 rseg_nested <- ds$get_json_prop(rseg_model$pid)
 
 riverseg_feature$propvalues()
-gage_mname = "01646000_PM7_4581_4580"
+
+
+gage_mname = "01631000_PS5_5240_5200"
 gage_model <- RomProperty$new(
   ds, list(
     featureid=riverseg_feature$hydroid, 
