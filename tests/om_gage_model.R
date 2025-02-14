b9cf97779d47b964fc9d82f42710b00e52aa0717
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2025-02-14 11:36:31 -0500

    added gage model creator update and handling date overlaps for better zoo

--- tests/om_gage_model.R
+++ tests/om_gage_model.R
@@ -18,6 +18,10 @@ gage_nested <- ds$get_json_prop(gage_model$pid)
 gage_nested <- ds$get_json_prop(7701223)
 
 # for testing R om summary 
-argst <- c(7700740, "01646000_PM7_4581_4580", 400, "http://deq1.bse.vt.edu:81/usgs/400/01646000_PM7_4581_4580.out")
+argst <- c(7700740, "01625000_PS3_6460_6230", 400, "/media/model/met/usgs/usgs/01625000_PS3_6460_6230.out")
+argst <- c(7701272, "01619500_PU2_3090_4050", "usgs", "http://deq1.bse.vt.edu:81/usgs/usgs/01619500_PU2_3090_4050.out")
+
 # for testing the scaling method
 argst <- c("01646000", "http://deq1.bse.vt.edu:81/usgs/400/01646000_PM7_4581_4580.out", "1984-01-01", "2020-12-31", 58.24)
+
+
