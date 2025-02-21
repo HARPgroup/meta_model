f7fdeedd64b13c0f179fe1f049f7c70c7fae46bc
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2025-02-21 11:35:32 -0500

    handle parent prop retrieval

--- scripts/rest/set_property.R
+++ scripts/rest/set_property.R
@@ -9,27 +9,31 @@ suppressPackageStartupMessages(library(jsonlite)) #for exporting values as json
 # Accepting command arguments:
 # argst = c("dh_properties", 7700740, "drainage_area", "propvalue")
 argst <- commandArgs(trailingOnly = T)
-if (length(argst) < 4) {
-  message("Use: set_property.R entity_type entity_id propname propvalue [propcode] [data_matrix (json string)]")
+if (length(argst) < 5) {
+  message("Use: set_property.R entity_type entity_id varkey (can use 'auto') propname propvalue [propcode] [data_matrix (json string)]")
   q()
 }
 entity_type <- argst[1]
 entity_id <- as.integer(argst[2])
-propname <- argst[3]
-propvalue <- argst[4]
+varkey <- argst[3]
+if (varkey == 'auto') {
+  varkey = NULL # uses guessing feature of routine
+}
+propname <- argst[4]
+propvalue <- argst[5]
 if (propvalue == "NA") {
   propvalue = NA
 }
 propcode = NA
-if (length(argst) > 4) {
-  propcode <- argst[5]
+if (length(argst) > 5) {
+  propcode <- argst[6]
   if (propcode == "NA") {
     propcode = NA
   }
 }
 data_matrix=NA
-if (length(argst) > 5) {
-  data_matrix <- argst[6]
+if (length(argst) > 6) {
+  data_matrix <- argst[7]
   if (data_matrix == "NA") {
     data_matrix = NA
   }
@@ -50,10 +54,10 @@ if (is.na(parent$pid)) {
   q()
 }
 if (!is.na(propvalue)) {
-  parent$set_prop(propname,propvalue=propvalue)
+  parent$set_prop(propname,propvalue=propvalue,varkey=varkey)
 }
 if (!is.na(propcode)) {
-  parent$set_prop(propname,propcode=propcode)
+  parent$set_prop(propname,propcode=propcode,varkey=varkey)
 }
 if (!is.na(data_matrix)) {
   parent$set_matrix(propname,jsonlite::fromJSON(data_matrix))
