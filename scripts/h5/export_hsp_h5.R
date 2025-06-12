ed09a5e393c800d6b087dab3a672e39fcaf0eced
Robert W. Burgholzer <rburghol@vt.edu>
2025-06-12 11:05:45 +0000

    consistent var names

--- scripts/h5/export_hsp_h5.R
+++ scripts/h5/export_hsp_h5.R
@@ -1,6 +1,7 @@
 
 # Script for exporting run data from an h5 to CSV
 # Script will have 3 inputs: h5_file_path, output_file_path, and data_source_table 
+options(warn=-1)
 
 suppressPackageStartupMessages(library(rhdf5))
 suppressPackageStartupMessages(library(R.utils))
@@ -8,18 +9,18 @@ suppressPackageStartupMessages(library(R.utils))
 # Accepting command arguments:
 argst <- commandArgs(trailingOnly = T)
 if (length(argst) < 3) {
-  message("Use: Rscript export_hsp_h5.R h5_file_path output_file_path data_source_table");
+  message("Use: Rscript export_hsp_h5.R h5_file_path output_file_path data_source_path");
   message("Ex: Rscript export_hsp_h5.R soyN51069.h5 soyN51069.csv /RESULTS/PERLND_P001/PWATER/table")
   q()
 }
 h5_file_path <- argst[1] # arguments default as class 'character' ; name of the .h5
 output_file_path <- argst[2] #name of csv being created
-data_source_table <- argst[3] # Path to data table within h5 that begins with /[groupname]
+data_source_path <- argst[3] # Path to data table within h5 that begins with /[groupname]
 
 # Reading in table from h5
 fid = H5Fopen(h5_file_path) # Opens the h5 file, fid is a h5 identifier
-did = H5Dopen(fid, data_source_table) # Opens the data table of interest using the path provided
 data <- h5read(fid,data_source_path, bit64conversion = "double")
+H5close()
 data <- data$table
 origin <- "1970-01-01"
 data$index <- as.POSIXct((data$index)/10^9, origin = origin, tz = "UTC")
