f299cf6fe4ff1a00d8db4be87dbfb61c041323b5
Rob <rburghol@vt.edu>
2026-07-21 23:56:45 +0000

    added latest version of norain

--- scripts/usgs/deq_norain.R
+++ scripts/usgs/deq_norain.R
@@ -2,20 +2,40 @@
 library("hydrotools")
 library("agws")
 library("dataRetrieval")
-basedir = "http://deq1.bse.vt.edu:81/usgs/agws/"
+basepath='/var/www/R';
+source(paste(basepath,'config.R',sep='/'))
 
-gage_id = "01634000"
-glist = c(gage_id)
-g_roanoke = c("02065500", "02059500", "02056000","02054530", "02056900")
-glist = c(
-  "03524000", "03167000", "01674500", "01667500",
-  "01654000",  "01634000", "02016000", "02039500", "02042500", "02051500",
-  "02059500", "02056000","02054530", "02056650", "02056900"
-)
+argst <- commandArgs(trailingOnly=T)
+# Ex:
+# argst = c("02065500,02059500,02056000,02054530,02056900,02058400,02071000,02061500,02064000", '/tmp/test.csv')
+# argst = c("02065500,02059500,02056000,02054530,02056900", '/tmp/test.csv', "2002-07-10")
+# argst = c("03524000,03167000,01674500,01667500,01654000,01634000,02016000,02039500,02042500,02051500,02059500,02056650", '/tmp/test.csv')
+
+message(paste("length of argst = ", length(argst)))
+if (length(argst) < 2) {
+  message(paste("Use: deq_norain.R output_file gages( \"02065500,02059500,...\") [start_date] [end_date]"))
+  q()
+}
+gages <- as.character(argst[1])
+glist <- stringr::str_split(gages,",",simplify=TRUE)
+# get or guess the date to aim for projection
+save_path = argst[2]
+if (length(argst) > 2) {
+  proj_start_date = argst[3]
+} else {
+  proj_start_date = format(Sys.time(), "%Y-%m-%d")
+}
+if (length(argst) > 3) { 
+  proj_end_date = argst[4]
+} else {
+  proj_end_date = format(as.Date(proj_start_date) + 90, "%Y-%m-%d")
+}
+yr = year(proj_start_date)
+
 # Notes:
 # - "02075500" Dan River at Paces VA is too influenced by 50 cfs flowby from Smith River to use
-g_list = g_roanoke
 odf <- data.frame(
+  hydroid = integer(),
   gage_id = character(),
   gage_name = character(),
   norain_90 = numeric(),
@@ -31,7 +51,7 @@ for (gage_id in glist) {
   hydrocode = paste0('usgs_ws_', gage_id)
   omgage <- hydrotools::WaterGageDaily$new(ds_in = ds, gage_id = gage_id)
   omgage$load_wshd_feat()
-  omgage$get_gage_data_old(start_date = '1900-01-01', end_date='2026-06-28', approval_status = 'all')
+  omgage$get_gage_data_old(start_date = '1900-01-01', end_date=proj_end_date, approval_status = 'all')
   omgage$plot_low_flows()
   omgage$low_flows
   # Load model object for retrieving BPJ AGWRC
@@ -59,18 +79,16 @@ for (gage_id in glist) {
   # inspect for start date
   plot(
     Flow ~ Date, 
-    data=omgage$gage_data[omgage$gage_data$Date >= "2026-05-15",],
-    main=paste("Observed", model$feature$name)
+    data=omgage$gage_data[omgage$gage_data$Date >= (as.Date(proj_start_date) - 30),],
+    main=paste("Observed", model$feature$name),
+    ylim=c(0, max(omgage$gage_data[omgage$gage_data$Date >= (as.Date(proj_start_date) - 30),]$Flow))
   )
   days = nrow(omgage$gage_data)
   last30 = omgage$gage_data[(days - 30):days,]
   Q0 = min(last30$Flow)
   start_date = max(last30[last30$Flow == Q0,]$Date)
   points(start_date, Q0, col="red", bg="red", pch = 21, cex = 2)
-  # load the gage regression info from the server
-  #es = agws::analyze_recession(eventurl)
-  #regurl = paste0(basedir, "baseflow_regression_df_", omgage$gage_id, ".csv")
-  #reg = read.csv(regurl)
+  # load the gage regression info from the database
   if (is.na(l90_agwrc$pid)) {
     #Ce = agws::RegressionAGWRC(Flow = Q0, m = reg$m[1], b = reg$b[1])
     Ce = NA
@@ -113,6 +131,8 @@ for (gage_id in glist) {
     AGWRC = AGWRC
   )
   print(bff) # display ggplot
+  fpath = paste0(save_directory, "/Q90_norain_log_", gage_id, yr, "_", ".png")
+  ggplot2::ggsave(fpath)
   if (!is.na(Ce)) {
     if (method != 'regression_limit') {
       # use a manually defined constant C
@@ -130,7 +150,8 @@ for (gage_id in glist) {
   end_date <- fc[90,]$Date
   is_emerg = 'No' 
   is_hist = 'No'
-  if (Q90 <= min(omgage$low_flows$n1Q10_annDate$minFlow)) {
+  Qmin = min(omgage$low_flows$n1Q10_annDate$minFlow)
+  if (Q90 <= Qmin) {
     is_hist = 'Yes' # look up from percentile tables
   }
   ntab <- omgage$nep_table()
@@ -139,13 +160,28 @@ for (gage_id in glist) {
   if (Q90 <= emo_em) {
     is_emerg = 'Yes'
   }
+  yscale = max(fc$Forecast,na.rm=TRUE)
+  yinc = yscale / 10
   plot(
     fc$Forecast ~ fc$Date,
     main=paste("Projected", model$feature$name)
   )
-  Ce = median(fc$AGWRC)
+  text(as.Date(end_date - 10), Q90 + yinc * 2, paste("Q90 =", round(Q90,1), "cfs"))
+  text(as.Date(end_date - 10), Q90 + yinc * 3, paste("Qmin =", round(Qmin,1), "cfs"))
+  fpath = paste0(save_directory, "/Q90_norain_", gage_id, yr, "_", ".png")
+  png(fpath)
+  # now save the same thing
+  plot(
+    fc$Forecast ~ fc$Date,
+    main=paste("Projected", model$feature$name)
+  )
+  text(as.Date(end_date - 10), Q90 + yinc * 2, paste("Q90 =", round(Q90,1), "cfs"))
+  text(as.Date(end_date - 10), Q90 + yinc * 3, paste("Qmin =", round(Qmin,1), "cfs"))
+  dev.off()
+    Ce = median(fc$AGWRC)
   odl <- data.frame (
-    age_id = gage_id,
+    hydroid = omgage$gage_feature$hydroid,
+    gage_id = gage_id,
     gage_name = model$feature$name,
     norain_90 = Q90,
     proj_date = end_date,
@@ -166,4 +202,4 @@ for (gage_id in glist) {
 
 # other functions:
 # agws::fit_agwrc_regression(events)
-
+write.csv(odf,file=save_path)
