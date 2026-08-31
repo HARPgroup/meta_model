fae31718ca3c9e664dee0eed90d5147f1d2bf4f5
Connor Brogan <connor.brogan@deq.virginia.gov>
2026-08-31 16:08:44 -0400

    Unique drought exit handlers

---
+++ scripts/qa/R/agws_00_exithandler.R
@@ -0,0 +1,35 @@
+#Creates the appropriate model and scenario properties for a USGS feature and
+#stores regression coefficients
+#For local testing:
+# commandArgs <- function(...){
+#   c("01613900")
+# }
+
+#Load in hydrotools and connect to db
+library(hydrotools)
+suppressPackageStartupMessages(library(stringr))
+basepath='/var/www/R'
+source('/var/www/R/config.R')
+
+argst <- commandArgs(trailingOnly = T)
+message(paste0("DEBUG with: argst <- c('",paste(argst,collapse="', '")),"')")
+#The USGS gage id
+gage_id <- argst[1]
+#Create gage object, no need to pull in real data as we just need the properties
+#so we initialize with fake data
+gage_obj <- hydrotools::WaterGageDaily$new(gage_id = gage_id, ds_in = ds,
+                                           data_source = data.frame(time = 0, value = 0),
+                                           flow_col = "value", date_col = "time")
+
+agws_props <- gage_obj$agwrc_fun()
+
+if(all(agws_props$propname != "rating_class")){
+  gage_obj$save_baseflow_context(data_type = "gage_conclusions",
+                                 study_agwrc_method = 7,
+                                 study_context = "AGWS workflow failed.")
+}else{
+  message("No exit_handler rating_class property set as rating_class is already set as",
+          agws_props$propvalue[agws_props$propname == "rating_class"])
+}
+
+
---
+++ scripts/qa/R/agws_05_exithandler.R
@@ -0,0 +1,35 @@
+#For local testing:
+# commandArgs <- function(...){
+#   c("01613900")
+# }
+
+#Load in hydrotools and connect to db
+library(hydrotools)
+suppressPackageStartupMessages(library(stringr))
+basepath='/var/www/R'
+source('/var/www/R/config.R')
+
+argst <- commandArgs(trailingOnly = T)
+message(paste0("DEBUG with: argst <- c('",paste(argst,collapse="', '")),"')")
+#The USGS gage id
+gage_id <- argst[1]
+
+gage_obj <- hydrotools::WaterGageDaily$new(gage_id = gage_id, ds_in = ds,
+                                           data_source = data.frame(time = 0, value = 0),
+                                           flow_col = "value", date_col = "time")
+
+agws_props <- gage_obj$agwrc_fun()
+
+if(all(agws_props$propname != "rating_class")){
+  gage_obj$save_baseflow_context(data_type = "gage_conclusions",
+                                 study_agwrc_method = 6,
+                                 study_context = "QC failed in AGWS workflow
+                                 when trying to establish a relationship
+                                 between flow and AGWRC, likely due to lack of
+                                 events in Analyze Step 08-09.")
+}else{
+  message("No exit_handler rating_class property set as rating_class is already set as",
+          agws_props$propvalue[agws_props$propname == "rating_class"])
+}
+
+
